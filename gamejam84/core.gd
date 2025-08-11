15510ed974ed8f6a045b3d52f278cf810a29c08b
Evenwood <darienro3ach.37@gmail.com>
2025-08-11 11:24:18 -0400

    Added a hud that displays the score, time remaining, and provides a start button; additionally, added a global "core" script that will contain variables accessible by scene. Currently it contains the time limit and the message displayed on game start.

---
+++ gamejam84/core.gd
@@ -0,0 +1,4 @@
+extends Node
+
+const TIME_LIMIT = 120
+const MAIN_MESSAGE = "Squish the Critters!!!"
--- gamejam84/main.gd
+++ gamejam84/main.gd
@@ -2,10 +2,11 @@ extends Node
 
 @export var mob_scene: PackedScene
 var score
+var time
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,9 +17,14 @@ func _process(delta: float) -> void:
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
 	score = 0
+	time = 0
+	$HUD.update_score(score)
+	$HUD.update_time(time)
+	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 
@@ -50,6 +56,11 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	time += 1
+	$HUD.update_score(score)
+	$HUD.update_time(time)
+	if(time >= Core.TIME_LIMIT):
+		game_over()
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
