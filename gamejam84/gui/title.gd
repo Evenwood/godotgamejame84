d10891825c234ccbde2fac45511b128bf6e2bb44
Evenwood <darienro3ach.37@gmail.com>
2025-08-12 16:11:19 -0400

    Added the game title to the Core script as a constant variable, added a tweening effect to the title label

--- gamejam84/gui/title.gd
+++ gamejam84/gui/title.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+@onready var tween: = get_tree().create_tween()
+@onready var title: = $CanvasLayer/TitleLabel
+
+func _ready() -> void:
+	var length = Core.TITLE.length()
+	tween.tween_property(title, "visible_characters", length, 2)
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main.tscn")
