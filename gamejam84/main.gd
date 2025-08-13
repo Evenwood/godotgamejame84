a23a67fd42f7df7ea168a7ad33464b9445c8e0b2
Dieboldcu <76053057+Dieboldcu@users.noreply.github.com>
2025-08-12 21:37:31 -0400

    Updated the splat animation

--- gamejam84/main.gd
+++ gamejam84/main.gd
@@ -61,7 +61,7 @@ func _on_mob_timer_timeout() -> void:
 	mob.rotation = direction
 
 	# Choose the velocity for the mob.
-	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	var velocity = Vector2(randf_range(100.0, 350.0), 0.0)
 	mob.linear_velocity = velocity.rotated(direction)
 
 	# Spawn the mob by adding it to the Main scene.
